13a3642fd017441d4587af934e675626e948fdc5
SionSmallman <32775963+SionSmallman@users.noreply.github.com>
2024-09-24 07:27:33 +0100

    Compressed hero images to improve initial pageload times

--- src/components/HomeComponents/HeroComponents/HeroImage.tsx
+++ src/components/HomeComponents/HeroComponents/HeroImage.tsx
@@ -3,8 +3,8 @@
 export default function HeroImage() {
   return (
     <div className='flex flex-col h-full items-center animate-dropInTop [&_img]:max-w-[100%] [&_img]:h-auto'>
-        <img className="rotate-6 translate-y-5 shadow-2xl" src="/static/images/setlistfm-demo.png" alt="Setlist.fm demo image"></img>
-        <img className="-rotate-3 shadow-xl" src="/static/images/spotify-demo.png" alt="Spotify demo image"></img>
+        <img className="rotate-6 translate-y-5 shadow-2xl" src="/static/images/setlistfm-demo.webp" alt="Setlist.fm demo image"></img>
+        <img className="-rotate-3 shadow-xl" src="/static/images/spotify-demo.webp" alt="Spotify demo image"></img>
     </div>
   )
 }
