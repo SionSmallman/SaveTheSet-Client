667fe1cbd0a95250b2d6ad12d7620cb07e8679ed
SionSmallman <32775963+SionSmallman@users.noreply.github.com>
2024-10-07 20:56:04 +0100

    Replaced useEffect api fetch for RecentlySavedPlaylists in favour of React Query

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthContextProvider } from "./context/AuthContext";
 import { ProtectedRoute } from "./components/AuthComponents/ProtectedRoute";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 //pages
 import Home from "./pages/Home";
@@ -11,37 +12,41 @@ import UserSettings from "./pages/UserSettings";
 import UserHistory from "./pages/UserHistory";
 import NotFoundPage from "./pages/NotFound";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <AuthContextProvider>
-          <div className="pages">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/s/:sid" element={<SetlistPage />} />
-              <Route path="/authredirect" element={<RedirectWindow />} />
-              <Route
-                path="/settings"
-                element={
-                  <ProtectedRoute>
-                    <UserSettings />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/history"
-                element={
-                  <ProtectedRoute>
-                    <UserHistory />
-                  </ProtectedRoute>
-                }
-              />
-              <Route path='*' element={<NotFoundPage />} />
-            </Routes>
-          </div>
-        </AuthContextProvider>
-      </BrowserRouter>
+      <QueryClientProvider client={ queryClient }>
+        <BrowserRouter>
+          <AuthContextProvider>
+            <div className="pages">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/s/:sid" element={<SetlistPage />} />
+                <Route path="/authredirect" element={<RedirectWindow />} />
+                <Route
+                  path="/settings"
+                  element={
+                    <ProtectedRoute>
+                      <UserSettings />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/history"
+                  element={
+                    <ProtectedRoute>
+                      <UserHistory />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route path='*' element={<NotFoundPage />} />
+              </Routes>
+            </div>
+          </AuthContextProvider>
+        </BrowserRouter>
+      </QueryClientProvider>
     </div>
   );
 }
